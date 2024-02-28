28c1ee5b82f303eced3b710be116fadb50804629
jx1279 <jx1279@nyu.edu>
2024-02-28 14:07:34 -0500

    Updated Death Scene Code so it lets you go once youve survived for 1 minute

--- Assets/Scripts/EscapedDeath.cs
+++ Assets/Scripts/EscapedDeath.cs
@@ -7,12 +7,10 @@ public class EscapedDeath : MonoBehaviour
 {
 
     public float timeLeft = 60.0f;
-    public Text startText;
 
     void Update()
     {
         timeLeft -= Time.deltaTime;
-        startText.text = (timeLeft).ToString("0");
         if (timeLeft < 0)
         {
             SceneManager.LoadScene("Level");
