166e84376606f1f784971040688331c80ddc94a9
Kewl <87680132+ac8736@users.noreply.github.com>
2024-02-27 22:24:13 -0500

    player shoot

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Despawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float bulletForce = 17;
+    public Transform firePoint;
+
+    private bool m_CanShoot = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +35,40 @@ public class PlayerWeapon : MonoBehaviour
         // Rotate the sprite to face the mouse
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
+        if (m_CanShoot)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                StartCoroutine(ShootCD());
+                Shoot();
+            }
+        }
+    }
 
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+
+        // Calculate direction towards mouse
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new(
+            mousePos.x - transform.position.x,
+            mousePos.y - transform.position.y
+        );
+
+        // Normalize direction to get a unit vector
+        direction.Normalize();
+
+        // Set bullet's velocity to travel in the calculated direction
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = direction * bulletForce;
+    }
+
+    IEnumerator ShootCD()
+    {
+        m_CanShoot = false;
+        yield return new WaitForSeconds(0.1f);
+        m_CanShoot = true;
     }
 }
