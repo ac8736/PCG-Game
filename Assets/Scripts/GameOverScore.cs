eace0544e0ffeda3fabc8b2dee7cc7a5d6fd9f84
greenapple18 <114010133+greenapple18@users.noreply.github.com>
2024-03-27 10:17:28 -0400

    Update game over screen with score board,, update death scene with tracking mechanism and updated UI sprites
    Update game over screen with score board,, update death scene with tracking mechanism and updated UI sprites

---
+++ Assets/Scripts/GameOverScore.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverScore : MonoBehaviour
+{
+    public TextMeshProUGUI finalScoreText;
+    void Start()
+    {
+        finalScoreText.text = "Final Score: " + publicvar.playerScore;
+    }
+
+    
+}
