d1b0d305d6ab8b0bd637d9f20008ee71b13f1c86
semsemihere <sh6042@nyu.edu>
2024-04-16 04:08:59 -0400

    Start screen update

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+
+    private float speed = 3f;
+    private float minX, maxX;
+
     // Start is called before the first frame update
     public void PlayGame()
     {
@@ -25,4 +29,40 @@ public class StartGame : MonoBehaviour
         Application.Quit();
         Debug.Log("Application QUIT");
     }
+
+    private void Start()
+    {
+        SetMovementBounds();
+    }
+
+    private void Update()
+    {
+        gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x > maxX || transform.position.x < minX)
+        {
+            speed *= -1;
+
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            Debug.Log("CHANGING");
+        }
+
+    }
+
+    private void SetMovementBounds()
+    {
+        Camera mainCamera = Camera.main;
+        if (mainCamera.orthographic)
+        {
+            float screenHalfWidthInWorldUnits = mainCamera.orthographicSize * Screen.width / Screen.height;
+            minX = mainCamera.transform.position.x - screenHalfWidthInWorldUnits;
+            maxX = mainCamera.transform.position.x + screenHalfWidthInWorldUnits;
+        }
+        else
+        {
+            Debug.LogError("Camera is not orthographic, script requires an orthographic camera to function properly.");
+        }
+    }
+
+
 }
