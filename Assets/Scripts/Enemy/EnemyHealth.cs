a344ff6c795a34c5e9051d701a99c0b1faf54588
Kewl <87680132+ac8736@users.noreply.github.com>
2024-04-30 18:28:46 -0400

    dead animations to enemies

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public EnemyStats m_EnemyStats; 
+    public EnemyStats m_EnemyStats;
     public Animator m_Animator;
     public bool m_CanDamage = false;
     public FloatingHealthbar m_FloatingHealthbar;
+    public GameObject m_Weapon;
+    public AIPath m_AIPath;
 
     public bool m_Spin, m_Chase, m_Single;
 
     private int m_Health;
+    private bool m_IsDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +32,20 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (m_Health <= 0)
+        if (m_Health <= 0 && !m_IsDead)
         {
-            GetComponent<EnemyRoomHandler>().RemoveFromRoomList();
-            Destroy(gameObject);
+            m_IsDead = true;
+            if (m_Weapon != null) { m_Weapon.SetActive(false); }
+            if (m_AIPath != null) { m_AIPath.canMove = false; }
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            m_Animator.SetTrigger("Death");
         }
+    }
+
+    public void Die()
+    {
+        GetComponent<EnemyRoomHandler>().RemoveFromRoomList();
+        Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
