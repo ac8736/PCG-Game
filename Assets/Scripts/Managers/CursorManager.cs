45075ee9c7008dc1e7165f74f44d1dd4541aba8e
Kewl <87680132+ac8736@users.noreply.github.com>
2024-05-01 17:25:16 -0400

    changing crosshairs

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -5,14 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class CursorManager : MonoBehaviour
 {
-    public Texture2D m_MenuCursorTexture; 
+    public Texture2D m_MenuCursorTexture;
+    public Texture2D m_GameCursorTexture;
     public Vector2 m_HotSpot = Vector2.zero;
 
     void Start()
     {
         Scene currentScene = SceneManager.GetActiveScene();
 
-        if (currentScene.name == "StartScene")
+        if (currentScene.name == "StartScene" || currentScene.name == "GameWin" || currentScene.name == "GameOver" || currentScene.name == "LevelToDeathTransition")
             Cursor.SetCursor(m_MenuCursorTexture, m_HotSpot, CursorMode.Auto);
+        if (currentScene.name == "Level" || currentScene.name == "Death")
+            Cursor.SetCursor(m_GameCursorTexture, m_HotSpot, CursorMode.Auto);
     }
 }
