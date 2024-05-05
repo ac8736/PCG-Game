7b7ea5edefd0a248e7efc42839fb8853b828ed80
ac8736 <87680132+ac8736@users.noreply.github.com>
2024-05-04 23:07:35 -0400

    added beginner context text

---
+++ Assets/Scripts/Text/FlavorText.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlavorText : MonoBehaviour
+{
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
